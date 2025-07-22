269ef51bfbf9669eaf8a3508d517205cbefc0842
lamaison22 <joaomiranda124@hotmail.com>
2025-07-22 11:36:20 -0300

    crop images service

--- src/GameCard/GameCard.tsx
+++ src/GameCard/GameCard.tsx
@@ -1,6 +1,7 @@
 import CriticScore from "@/components/CriticScore.tsx/CriticScore";
 import PlatformIconList from "@/components/PlatformIconList/PlatformIconList";
 import { Game } from "@/hooks/useGames";
+import getCroppedImage from "@/services/crop-image";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 interface Props {
@@ -10,7 +11,7 @@ function GameCard({ game }: Props) {
   return (
     <>
       <Card.Root borderRadius="10px" overflow={"hidden"}>
-        <Image src={game.background_image} />
+        <Image src={getCroppedImage(game.background_image)} />
         <Card.Body gap="2">
           <Heading>{game.name}</Heading>
           <HStack justifyContent={"space-between"}>
